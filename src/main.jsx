1a343d060466bc2b7eb372c396fb6d2cb854b1cd
gwynbleiddrivia <yasinzme2@gmail.com>
2023-08-11 20:30:53 +0600

    footer finished

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from './App.jsx'
+import './index.css'
+
+import Main from './layout/Main'
+import Home from './pages/Home'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+	{
+		path: "/",
+		element: <Home></Home>
+	}
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  	<div className='mx-auto w-full max-w-6xl'>
+      	<RouterProvider router={router} />
+	</div>
+  </React.StrictMode>,
+)
