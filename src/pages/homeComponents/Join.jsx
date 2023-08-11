26788deb440fa94688ea0523029e2c69067efe39
gwynbleiddrivia <yasinzme2@gmail.com>
2023-08-12 05:57:01 +0600

    join added

--- src/pages/homeComponents/Join.jsx
+++ src/pages/homeComponents/Join.jsx
@@ -2,7 +2,10 @@
 const Join = () => {
 	return (
 		<div>
-			<h1>This is Join</h1>
+			<div className="join-background flex flex-col gap-8 text-center my-auto py-36 px-80 mt-20 rounded-lg">
+				<p className="text-white text-3xl font-bold">Join our member and get <br/>discount up to 50%</p>
+				<button className="yellow-color px-10 py-2 border-signin">Sign Up</button>
+			</div>
 
 		</div>
 	);
